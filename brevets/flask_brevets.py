aa8dcdd55431a91ecd42c7918b4afa3efdae1c16
Ethan Robb <ethanr.lax@gmail.com>
2023-11-08 20:34:22 -0800

    Final Commit: Logic/Flask/Tests

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -1,7 +1,7 @@
 """
 Replacement for RUSA ACP brevet time calculator
 (see https://rusa.org/octime_acp.html)
-
+Ethan Robb
 """
 
 import flask
@@ -9,7 +9,6 @@ from flask import request
 import arrow  # Replacement for datetime, based on moment.js
 import acp_times  # Brevet time calculations
 import config
-
 import logging
 
 ###
@@ -22,19 +21,16 @@ CONFIG = config.configuration()
 # Pages
 ###
 
-
 @app.route("/")
 @app.route("/index")
 def index():
     app.logger.debug("Main page entry")
     return flask.render_template('calc.html')
 
-
 @app.errorhandler(404)
 def page_not_found(error):
     app.logger.debug("Page not found")
     return flask.render_template('404.html'), 404
-
 
 ###############
 #
@@ -42,6 +38,7 @@ def page_not_found(error):
 #   These return JSON, rather than rendering pages.
 #
 ###############
+
 @app.route("/_calc_times")
 def _calc_times():
     """
@@ -51,17 +48,26 @@ def _calc_times():
     """
     app.logger.debug("Got a JSON request")
     km = request.args.get('km', 999, type=float)
+
+    # Brevet distance & start date
+    start_date = request.args.get('start_date', type = str)
+    brev_distance = request.args.get('brev_dist', 200, type=int) 
     app.logger.debug("km={}".format(km))
     app.logger.debug("request.args: {}".format(request.args))
-    # FIXME!
-    # Right now, only the current time is passed as the start time
-    # and control distance is fixed to 200
-    # You should get these from the webpage!
-    open_time = acp_times.open_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, 200, arrow.now().isoformat).format('YYYY-MM-DDTHH:mm')
-    result = {"open": open_time, "close": close_time}
-    return flask.jsonify(result=result)
-
+    begin_arrow = arrow.get(start_date) # Arrow variable from the beginning time
+    
+    try:
+        open_time = acp_times.open_time(km, brev_distance, begin_arrow).format('YYYY-MM-DDTHH:mm')
+        close_time = acp_times.close_time(km, brev_distance, begin_arrow).format('YYYY-MM-DDTHH:mm')
+        rslt = {"open": open_time, "close": close_time, "ecode": 0}
+        return flask.jsonify(result=rslt)
+    
+    except OverflowError:
+        rslt = {"open": "mm/dd/yyyy --:-- --", "close": "mm/dd/yyyy --:-- --", "ecode": 1}
+        return flask.jsonify(result=rslt)
+    except ArithmeticError:
+        rslt = {"open": "mm/dd/yyyy --:-- --", "close": "mm/dd/yyyy --:-- --", "ecode": 2}
+        return flask.jsonify(result=rslt)
 
 #############
 
--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -1,11 +1,96 @@
 """
 Nose tests for acp_times.py
-
 Write your tests HERE AND ONLY HERE.
+Ethan Robb
 """
-
+from acp_times import open_time, close_time
+import arrow
 import nose    # Testing framework
 import logging
+
 logging.basicConfig(format='%(levelname)s:%(message)s',
                     level=logging.WARNING)
 log = logging.getLogger(__name__)
+
+START_TIME1 = '2023-08-01T00:00' #GLOBAL
+
+def test_control_dist_set_0():
+    """
+    Test for when the control distance is set to 0
+    """
+    # Setup
+    start_time = arrow.get(START_TIME1)
+
+    open = open_time(0, 200, start_time)
+    close = close_time(0, 200, start_time)
+    
+    # Assert
+    assert open == start_time
+    assert close == start_time.shift(minutes=60)
+    return
+
+def test_open_function_regular():
+    """
+    Regular test cases for open_time function
+    """
+    # Setup
+    start_time = arrow.get(START_TIME1)
+
+    test1 = open_time(150, 200, start_time)
+    test2 = open_time(300, 300, start_time)
+    test3 = open_time(500, 600, start_time)
+
+    # Assert
+    assert test1 == start_time.shift(minutes=+265)
+    assert test2 == start_time.shift(minutes=+540)
+    assert test3 == start_time.shift(minutes=+928)
+    return
+    
+
+def test_open_function_edge():
+    """
+    Edge test cases for open_time function
+    """
+    # Setup
+    start_time = arrow.get(START_TIME1)
+
+    test1 = open_time(0, 200, start_time)
+    test2 = open_time(40, 200, start_time)
+    
+    # Assert
+    assert test1 == start_time
+    assert test2 == start_time.shift(minutes=+71)
+    return
+
+def test_close_function_regular():
+    """
+    Regular test cases for close_time function
+    """
+    # Setup
+    start_time = arrow.get(START_TIME1)
+
+    test1 = close_time(150, 200, start_time)
+    test2 = close_time(500, 600, start_time)
+    test3 = close_time(300, 300, start_time)
+   
+    # Assert
+    assert test1 == start_time.shift(minutes=+600)
+    assert test2 == start_time.shift(minutes=+2000)
+    assert test3 == start_time.shift(minutes=+1200)
+    return
+
+def test_close_function_edge():
+    """
+    Edge test cases for close_time function
+    """
+    # Setup
+    start_time = arrow.get(START_TIME1)
+
+    test1 = close_time(205, 200, start_time)
+    test2 = close_time(0, 200, start_time)
+
+    # Assert
+    assert test1 == start_time.shift(minutes=+800)
+    assert test2 == start_time.shift(minutes=+60)
+    return
+
